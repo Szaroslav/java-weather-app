a8d526a1b70215904a7799c605ecdf421a8b4be4
Suselkowy <jakubsus77@gmail.com>
2023-11-25 18:53:36 +0100

    add sample fxml

--- src/main/java/pl/edu/agh/to/weatherapp/App.java
+++ src/main/java/pl/edu/agh/to/weatherapp/App.java
@@ -1,15 +1,19 @@
 package pl.edu.agh.to.weatherapp;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class App extends Application  {
   @Override
-  public void start(Stage primaryStage) {
-    Scene scene = new Scene(new Label("test"));
-    primaryStage.setScene(scene);
+  public void start(Stage primaryStage) throws IOException {
+    Parent root = FXMLLoader.load(getClass().getResource("/view/AppMain.fxml"));
+    primaryStage.setScene(new Scene(root));
     primaryStage.show();
   }
 }
