90ded464f59af6c8a1b060e901e1e41c2f3c3e5f
Suselkowy <jakubsus77@gmail.com>
2023-11-27 22:10:30 +0100

    add gui tests

--- src/test/java/pl/edu/agh/to/weatherapp/gui/MockClasses/WeatherServiceMock.java
+++ src/test/java/pl/edu/agh/to/weatherapp/gui/MockClasses/WeatherServiceMock.java
@@ -1,18 +1,19 @@
-package pl.edu.agh.to.weatherapp.weather;
+package pl.edu.agh.to.weatherapp.gui.MockClasses;
 
 import lombok.SneakyThrows;
 import org.springframework.stereotype.Service;
 import pl.edu.agh.to.weatherapp.model.WeatherData;
+import pl.edu.agh.to.weatherapp.weather.WeatherService;
 
 @Service
 public class WeatherServiceMock implements WeatherService {
     @Override
     @SneakyThrows
     public WeatherData getWeatherData(String location) {
-        Thread.sleep(2000);
         WeatherData weatherData = new WeatherData();
         weatherData.setTemp(42);
         weatherData.setLocationName("Tarnów");
+        weatherData.setConditionIconUrl("https://www.save9.com/wp-content/uploads/2015/01/Cloud-Computing-from-Save9.png");
         return weatherData;
     }
 }
