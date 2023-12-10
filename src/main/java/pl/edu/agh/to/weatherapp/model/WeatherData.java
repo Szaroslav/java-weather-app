1638adad7e3e097c04b7383b6825435534390847
Jakub Szaredko <408530@student.agh.edu.pl>
2023-12-10 16:57:17 +0100

    Extend weather model

--- src/main/java/pl/edu/agh/to/weatherapp/model/WeatherData.java
+++ src/main/java/pl/edu/agh/to/weatherapp/model/WeatherData.java
@@ -1,12 +1,23 @@
 package pl.edu.agh.to.weatherapp.model;
 
+import java.util.Date;
+
+import lombok.NoArgsConstructor;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class WeatherData {
-    private int temp;
-    private String locationName;
+    private Date date;
     private String conditionIconUrl;
+
+    private int temperatureC;
+    private float windKph;
+    private float precipitationMm;
+    private boolean willRain;
+    private boolean willSnow;
 }
