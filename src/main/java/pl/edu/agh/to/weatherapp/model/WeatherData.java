4b8c0eeffb1e611e6e729c590c55f846905e5b19
Jakub Szaredko <408530@student.agh.edu.pl>
2023-12-10 16:44:15 +0100

    Add Lombook getter and setter annotations to the weather model

--- src/main/java/pl/edu/agh/to/weatherapp/model/WeatherData.java
+++ src/main/java/pl/edu/agh/to/weatherapp/model/WeatherData.java
@@ -1,31 +1,12 @@
 package pl.edu.agh.to.weatherapp.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
 public class WeatherData {
     private int temp;
     private String locationName;
     private String conditionIconUrl;
-
-    public int getTemp() {
-        return temp;
-    }
-
-    public void setTemp(int temp) {
-        this.temp = temp;
-    }
-
-    public String getLocationName() {
-        return locationName;
-    }
-
-    public void setLocationName(String locationName) {
-        this.locationName = locationName;
-    }
-
-    public String getConditionIconUrl() {
-        return conditionIconUrl;
-    }
-
-    public void setConditionIconUrl(String conditionIconUrl) {
-        this.conditionIconUrl = conditionIconUrl;
-    }
 }
