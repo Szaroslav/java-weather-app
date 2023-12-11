44d69ec35a3cd12f3c96f3850ee553f080b0bf74
Jeremiasz Śliwiński <410561@student.agh.edu.pl>
2023-12-11 21:36:45 +0100

    parser test

--- src/main/java/pl/edu/agh/to/weatherapp/parser/JsonParser.java
+++ src/main/java/pl/edu/agh/to/weatherapp/parser/JsonParser.java
@@ -14,68 +14,40 @@ import java.util.Date;
 
 public class JsonParser {
     private WeatherData weatherDataFromJsonObject(JsonObject json) {
-        Date date = new Date(json.getAsJsonPrimitive("time_epoch").getAsLong());
+        Date date = new Date(json.getAsJsonPrimitive("time_epoch").getAsLong() * 1000);
         String conditionIconUrl = json
-            .getAsJsonObject("condition")
-            .getAsJsonPrimitive("icon")
-            .getAsString();
-
-        int temperatureC      = json.getAsJsonPrimitive("temp_c").getAsInt();
-        float windKph         = json.getAsJsonPrimitive("wind_kph").getAsFloat();
+                .getAsJsonObject("condition")
+                .getAsJsonPrimitive("icon")
+                .getAsString();
+        int temperatureC = json.getAsJsonPrimitive("temp_c").getAsInt();
+        float windKph = json.getAsJsonPrimitive("wind_kph").getAsFloat();
         float precipitationMm = json.getAsJsonPrimitive("precip_mm").getAsFloat();
-        boolean willRain      = json.getAsJsonPrimitive("will_it_rain").getAsBoolean();
-        boolean willSnow      = json.getAsJsonPrimitive("will_it_snow").getAsBoolean();
-
+        boolean willRain = json.getAsJsonPrimitive("will_it_rain").getAsBoolean();
+        boolean willSnow = json.getAsJsonPrimitive("will_it_snow").getAsBoolean();
         return new WeatherData(
-            date,
-            conditionIconUrl,
-            temperatureC,
-            windKph,
-            precipitationMm,
-            willRain,
-            willSnow
+                date,
+                conditionIconUrl,
+                temperatureC,
+                windKph,
+                precipitationMm,
+                willRain,
+                willSnow
         );
     }
 
     public ForecastWeatherData parseForecast(String content) {
         JsonObject json = new Gson()
-            .fromJson(new String(
-                content.getBytes(),
-                StandardCharsets.UTF_8
-            ), JsonObject.class);
-
+                .fromJson(new String(content.getBytes(), StandardCharsets.UTF_8), JsonObject.class);
         if (json.has("error")) {
-            String errorMessage = json.getAsJsonObject("error")
-                .getAsJsonPrimitive("message")
-                .getAsString();
+            String errorMessage = json
+                    .getAsJsonObject("error")
+                    .getAsJsonPrimitive("message")
+                    .getAsString();
             throw new InvalidRequest(errorMessage);
         } else {
             return forecastWeatherFromJson(json);
         }
     }
-
-//    private WeatherData weatherDataFromJson(JsonObject json) {
-//        // Parse location details
-//        JsonObject locationJson = json.getAsJsonObject("location");
-//        String cityName = locationJson.getAsJsonPrimitive("name").getAsString();
-//        String country = locationJson.getAsJsonPrimitive("country").getAsString();
-//
-//        // Parse current weather details
-//        JsonObject currentJson = json.getAsJsonObject("current");
-//        double tempC = currentJson.getAsJsonPrimitive("temp_c").getAsDouble();
-//
-//        // Parse current weather condition details
-//        JsonObject conditionJson = currentJson.getAsJsonObject("condition");
-//        String conditionIconUrl = conditionJson.getAsJsonPrimitive("icon").getAsString();
-//
-//        // initialise WeatherData model
-//        WeatherData weatherData = new WeatherData();
-//        weatherData.setLocationName(cityName + ", " + country);
-//        weatherData.setTemperature((int) tempC);
-//        weatherData.setConditionIconUrl("https:" + conditionIconUrl);
-//
-//        return weatherData;
-//    }
 
     private ForecastWeatherData forecastWeatherFromJson(JsonObject json) {
         // Parse location details.
@@ -95,25 +67,22 @@ public class JsonParser {
             WeatherData weatherData = weatherDataFromJsonObject(object);
             forecastWeather.getHourlyWeatherForecasts().add(weatherData);
         }
-
         return forecastWeather;
     }
 
     private JsonArray hourlyWeatherFromJson(JsonObject json) {
         JsonArray dailyWeatherJsonArray = json
-            .getAsJsonObject("forecast")
-            .getAsJsonArray("forecastday");
+                .getAsJsonObject("forecast")
+                .getAsJsonArray("forecastday");
         if (dailyWeatherJsonArray.isEmpty()) {
             throw new InvalidRequest("Cannot fetch a forecast.");
         }
-
         JsonArray hourlyWeatherJsonArray = dailyWeatherJsonArray.get(0)
-            .getAsJsonObject()
-            .getAsJsonArray("hour");
+                .getAsJsonObject()
+                .getAsJsonArray("hour");
         if (hourlyWeatherJsonArray.isEmpty()) {
             throw new InvalidRequest("Cannot fetch a forecast.");
         }
-
         return hourlyWeatherJsonArray;
     }
 }
