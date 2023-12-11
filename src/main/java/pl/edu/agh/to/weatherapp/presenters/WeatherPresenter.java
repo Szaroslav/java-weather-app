798b66fb312c2fed756b2f0eb9d1b54eff339688
Jeremiasz Śliwiński <410561@student.agh.edu.pl>
2023-12-11 20:51:07 +0100

    fix

--- src/main/java/pl/edu/agh/to/weatherapp/presenters/WeatherPresenter.java
+++ src/main/java/pl/edu/agh/to/weatherapp/presenters/WeatherPresenter.java
@@ -101,6 +101,10 @@ public class WeatherPresenter {
         });
         hideWeatherInfo();
         clearErrorLabel();
+        locationLabel.setText("");
+        temperatureLabel.setText("");
+        precipitationLabel.setText("");
+        windLabel.setText("");
     }
 
     public void handleSearchAction() {
