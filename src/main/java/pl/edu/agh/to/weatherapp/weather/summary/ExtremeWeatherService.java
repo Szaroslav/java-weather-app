6e5f9fa04914f801d9ffc1f61313b5463fda48db
Kasl0 <slonieckacper@gmail.com>
2023-12-11 14:37:43 +0100

    Added streams for the state-of-the-art solution

--- src/main/java/pl/edu/agh/to/weatherapp/weather/summary/ExtremeWeatherService.java
+++ src/main/java/pl/edu/agh/to/weatherapp/weather/summary/ExtremeWeatherService.java
@@ -4,20 +4,12 @@ import pl.edu.agh.to.weatherapp.model.ForecastWeatherData;
 import pl.edu.agh.to.weatherapp.model.WeatherData;
 import pl.edu.agh.to.weatherapp.model.internal.*;
 
-import java.util.HashMap;
+import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 
 import dataprocessing.WeatherDataProcessing;
 
 public class ExtremeWeatherService implements WeatherSummaryService {
-    private int minTemperature = Integer.MAX_VALUE;
-    private float minApparentTemperature = Integer.MAX_VALUE;
-    private float maxWindInMps = 0;
-    private float maxPrecipitationMm = 0;
-    private boolean willRain = false;
-    private boolean willSnow = false;
-
     private TemperatureLevel getTemperatureLevel(int minTemperature) {
         if (20 <= minTemperature) return TemperatureLevel.HOT;
         else if (minTemperature <= 5) return TemperatureLevel.COLD;
@@ -38,54 +30,58 @@ public class ExtremeWeatherService implements WeatherSummaryService {
 
     @Override
     public InternalWeatherData getSummary(List<ForecastWeatherData> weatherDataList) {
-        initializeSummaryData(weatherDataList);
-        return initializeInternalWeatherData();
+        List<InternalWeatherData> summarisedLocations
+                = weatherDataList.stream().map(x->mapLocation(x.getHourlyWeatherForecasts())).toList();
+        return getExtremeWeather(summarisedLocations);
     }
 
-    private void initializeSummaryData(List<ForecastWeatherData> weatherDataList) {
-        for(ForecastWeatherData forecastWeatherData : weatherDataList) {
-            for(WeatherData weatherData : forecastWeatherData.getHourlyWeatherForecasts()) {
-                analyzeWeatherData(weatherData);
-            }
-        }
-    }
+    private InternalWeatherData mapLocation(List<WeatherData> weatherDataList) {
+        int minTemperature = weatherDataList.stream().map(WeatherData::getTemperatureC).min(Comparator.comparing(Integer::valueOf)).orElseThrow();
+        float minApparentTemperature = weatherDataList.stream().map(data -> WeatherDataProcessing.getApparentTemperature(data.getTemperatureC(), data.getWindKph())).min(Comparator.comparing(Float::valueOf)).orElseThrow();
+        float maxPrecipitationMm = weatherDataList.stream().map(WeatherData::getPrecipitationMm).max(Comparator.comparing(Float::valueOf)).orElseThrow();
+        float maxWindInMps = weatherDataList.stream().map(data -> WeatherDataProcessing.kphToMps(data.getWindKph())).max(Comparator.comparing(Float::valueOf)).orElseThrow();
+        boolean willRain = weatherDataList.stream().map(WeatherData::isWillRain).max(Comparator.comparing(Boolean::valueOf)).orElseThrow();
+        boolean willSnow = weatherDataList.stream().map(WeatherData::isWillSnow).max(Comparator.comparing(Boolean::valueOf)).orElseThrow();
 
-    private void analyzeWeatherData(WeatherData weatherData) {
-        minTemperature = Math.min(minTemperature, weatherData.getTemperatureC());
-        minApparentTemperature = Math.min(minApparentTemperature, WeatherDataProcessing.getApparentTemperature(weatherData.getTemperatureC(), weatherData.getWindKph()));
-        maxPrecipitationMm = Math.max(maxPrecipitationMm, weatherData.getPrecipitationMm());
-        maxWindInMps = Math.max(maxWindInMps, WeatherDataProcessing.kphToMps(weatherData.getWindKph()));
-        if (weatherData.isWillRain()) willRain = true;
-        if (weatherData.isWillSnow()) willSnow = true;
-    }
-
-    private InternalWeatherData initializeInternalWeatherData() {
         InternalWeatherData extremeWeatherData = new InternalWeatherData();
-
         extremeWeatherData.setTemperatureLevel(getTemperatureLevel((int) minApparentTemperature));
         extremeWeatherData.setTemperature(minTemperature);
         extremeWeatherData.setWindIntensity(getWindIntensity((int) maxWindInMps));
         extremeWeatherData.setWindInMps((int) maxWindInMps);
-        initializePrecipitationData(extremeWeatherData);
+        if (willRain && willSnow) {
+            extremeWeatherData.setPrecipitationType(PrecipitationType.BOTH);
+        }
+        else if (willRain) {
+            extremeWeatherData.setPrecipitationType(PrecipitationType.RAIN);
+        }
+        else if (willSnow) {
+            extremeWeatherData.setPrecipitationType(PrecipitationType.SNOW);
+        }
+        else {
+            extremeWeatherData.setPrecipitationType(PrecipitationType.NONE);
+        }
+        extremeWeatherData.setPrecipitationIntensity(getPrecipitationIntensity((int) maxPrecipitationMm));
+        extremeWeatherData.setPrecipitationInMm((int) maxPrecipitationMm);
 
         return extremeWeatherData;
     }
 
-    private void initializePrecipitationData(InternalWeatherData weatherData) {
-        if (willRain && willSnow) {
-            weatherData.setPrecipitationType(PrecipitationType.BOTH);
-        }
-        else if (willRain) {
-            weatherData.setPrecipitationType(PrecipitationType.RAIN);
-        }
-        else if (willSnow) {
-            weatherData.setPrecipitationType(PrecipitationType.SNOW);
-        }
-        else {
-            weatherData.setPrecipitationType(PrecipitationType.NONE);
-        }
+    private InternalWeatherData getExtremeWeather(List<InternalWeatherData> summarisedLocations) {
+        int minTemperature = summarisedLocations.stream().map(InternalWeatherData::getTemperature).min(Comparator.comparing(Integer::valueOf)).orElseThrow();
+        int maxPrecipitationMm = summarisedLocations.stream().map(InternalWeatherData::getPrecipitationInMm).max(Comparator.comparing(Integer::valueOf)).orElseThrow();
+        int maxWindInMps = summarisedLocations.stream().map(InternalWeatherData::getWindInMps).max(Comparator.comparing(Integer::valueOf)).orElseThrow();
+        TemperatureLevel temperatureLevel = summarisedLocations.stream().map(InternalWeatherData::getTemperatureLevel).max(Comparator.comparing(TemperatureLevel::ordinal)).orElseThrow();
+        PrecipitationType precipitationType = summarisedLocations.stream().map(InternalWeatherData::getPrecipitationType).max(Comparator.comparing(PrecipitationType::ordinal)).orElseThrow();
 
-        weatherData.setPrecipitationIntensity(getPrecipitationIntensity((int) maxPrecipitationMm));
-        weatherData.setPrecipitationInMm((int) maxPrecipitationMm);
+        InternalWeatherData extremeWeatherData = new InternalWeatherData();
+        extremeWeatherData.setTemperatureLevel(temperatureLevel);
+        extremeWeatherData.setTemperature(minTemperature);
+        extremeWeatherData.setWindIntensity(getWindIntensity(maxWindInMps));
+        extremeWeatherData.setWindInMps(maxWindInMps);
+        extremeWeatherData.setPrecipitationType(precipitationType);
+        extremeWeatherData.setPrecipitationIntensity(getPrecipitationIntensity(maxPrecipitationMm));
+        extremeWeatherData.setPrecipitationInMm(maxPrecipitationMm);
+
+        return extremeWeatherData;
     }
 }
