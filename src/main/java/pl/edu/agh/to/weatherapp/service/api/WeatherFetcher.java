8548e072a8e1aced96ebbe366902c24f757dd7b7
Jakub Szaredko <408530@student.agh.edu.pl>
2024-01-07 23:24:16 +0100

    Fetch weather history

--- src/main/java/pl/edu/agh/to/weatherapp/service/api/WeatherFetcher.java
+++ src/main/java/pl/edu/agh/to/weatherapp/service/api/WeatherFetcher.java
@@ -1,8 +1,10 @@
 package pl.edu.agh.to.weatherapp.service.api;
 
 import java.util.concurrent.CompletableFuture;
+import org.joda.time.DateTime;
 
 public interface WeatherFetcher {
     CompletableFuture<String> fetchCurrent(String locationName);
     CompletableFuture<String> fetchForecast(String locationName, int daysNumber);
+    CompletableFuture<String> fetchHistory(String locationName, DateTime date);
 }
